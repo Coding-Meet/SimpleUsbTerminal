1dbd39ad216932cf329b6919d172bfff814c320e
Coding Meet <131579912+Coding-Meet@users.noreply.github.com>
2025-02-24 19:24:38 +0530

    Update DevicesFragment.java
    - Save serial settings in shared preferences
    - Update setting options in terminal device list menu
    - Add new baud rates to baud rate list.

--- app/src/main/java/de/kai_morich/simple_usb_terminal/DevicesFragment.java
+++ app/src/main/java/de/kai_morich/simple_usb_terminal/DevicesFragment.java
@@ -3,6 +3,7 @@ package de.kai_morich.simple_usb_terminal;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.hardware.usb.UsbDevice;
 import android.hardware.usb.UsbManager;
 import android.os.Bundle;
@@ -17,10 +18,12 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.hoho.android.usbserial.driver.UsbSerialDriver;
+import com.hoho.android.usbserial.driver.UsbSerialPort;
 import com.hoho.android.usbserial.driver.UsbSerialProber;
 
 import java.util.ArrayList;
@@ -42,7 +45,6 @@ public class DevicesFragment extends ListFragment {
 
     private final ArrayList<ListItem> listItems = new ArrayList<>();
     private ArrayAdapter<ListItem> listAdapter;
-    private int baudRate = 19200;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -97,22 +99,64 @@ public class DevicesFragment extends ListFragment {
         if(id == R.id.refresh) {
             refresh();
             return true;
-        } else if (id ==R.id.baud_rate) {
-            final String[] baudRates = getResources().getStringArray(R.array.baud_rates);
-            int pos = java.util.Arrays.asList(baudRates).indexOf(String.valueOf(baudRate));
-            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-            builder.setTitle("Baud rate");
-            builder.setSingleChoiceItems(baudRates, pos, new DialogInterface.OnClickListener() {
-                public void onClick(DialogInterface dialog, int item) {
-                    baudRate = Integer.parseInt(baudRates[item]);
-                    dialog.dismiss();
-                }
-            });
-            builder.create().show();
+        } else if (id ==R.id.serial_setting) {
+            showSerialSettingsDialog();
             return true;
         } else {
             return super.onOptionsItemSelected(item);
         }
+    }
+    private void showSerialSettingsDialog() {
+        Context context = getActivity();
+        if (context == null) return;
+
+        final String[] baudRates = getResources().getStringArray(R.array.baud_rates);
+        final String[] dataBits = {"5", "6", "7", "8"};
+        final String[] stopBits = {"1", "1.5", "2"};
+        final String[] parity = {"None", "Odd", "Even", "Mark", "Space"};
+
+        SharedPreferences prefs = context.getSharedPreferences("usb_settings", Context.MODE_PRIVATE);
+        int selectedBaud = java.util.Arrays.asList(baudRates).indexOf(String.valueOf(prefs.getInt("baudRate", 19200)));
+        int selectedDataBits = java.util.Arrays.asList(dataBits).indexOf(String.valueOf(prefs.getInt("dataBits", UsbSerialPort.DATABITS_8)));
+        int selectedStopBits = java.util.Arrays.asList(stopBits).indexOf(String.valueOf(prefs.getInt("stopBits", UsbSerialPort.STOPBITS_1)));
+        int selectedParity = prefs.getInt("parity", UsbSerialPort.PARITY_NONE);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setTitle("Serial Settings");
+
+        View view = getLayoutInflater().inflate(R.layout.dialog_serial_settings, null);
+        Spinner baudRateSpinner = view.findViewById(R.id.spinner_baud);
+        Spinner dataBitsSpinner = view.findViewById(R.id.spinner_data_bits);
+        Spinner stopBitsSpinner = view.findViewById(R.id.spinner_stop_bits);
+        Spinner paritySpinner = view.findViewById(R.id.spinner_parity);
+
+        ArrayAdapter<String> adapterBaud = new ArrayAdapter<>(context, android.R.layout.simple_spinner_dropdown_item, baudRates);
+        ArrayAdapter<String> adapterDataBits = new ArrayAdapter<>(context, android.R.layout.simple_spinner_dropdown_item, dataBits);
+        ArrayAdapter<String> adapterStopBits = new ArrayAdapter<>(context, android.R.layout.simple_spinner_dropdown_item, stopBits);
+        ArrayAdapter<String> adapterParity = new ArrayAdapter<>(context, android.R.layout.simple_spinner_dropdown_item, parity);
+
+        baudRateSpinner.setAdapter(adapterBaud);
+        baudRateSpinner.setSelection(selectedBaud);
+        dataBitsSpinner.setAdapter(adapterDataBits);
+        dataBitsSpinner.setSelection(selectedDataBits);
+        stopBitsSpinner.setAdapter(adapterStopBits);
+        stopBitsSpinner.setSelection(selectedStopBits);
+        paritySpinner.setAdapter(adapterParity);
+        paritySpinner.setSelection(selectedParity);
+
+        builder.setView(view);
+        builder.setPositiveButton("Save", (dialog, which) -> {
+            SharedPreferences.Editor editor = prefs.edit();
+            editor.putInt("baudRate", Integer.parseInt(baudRates[baudRateSpinner.getSelectedItemPosition()]));
+            editor.putInt("dataBits", Integer.parseInt(dataBits[dataBitsSpinner.getSelectedItemPosition()]));
+            editor.putInt("stopBits", stopBitsSpinner.getSelectedItemPosition() == 0 ? UsbSerialPort.STOPBITS_1 :
+                    stopBitsSpinner.getSelectedItemPosition() == 1 ? UsbSerialPort.STOPBITS_1_5 :
+                            UsbSerialPort.STOPBITS_2);
+            editor.putInt("parity", paritySpinner.getSelectedItemPosition());
+            editor.apply();
+        });
+        builder.setNegativeButton("Cancel", null);
+        builder.show();
     }
 
     void refresh() {
@@ -141,10 +185,19 @@ public class DevicesFragment extends ListFragment {
         if(item.driver == null) {
             Toast.makeText(getActivity(), "no driver", Toast.LENGTH_SHORT).show();
         } else {
+            SharedPreferences prefs = getActivity().getSharedPreferences("usb_settings", Context.MODE_PRIVATE);
+            int baudRate = prefs.getInt("baudRate", 19200);
+            int dataBits = prefs.getInt("dataBits", UsbSerialPort.DATABITS_8);
+            int stopBits = prefs.getInt("stopBits", UsbSerialPort.STOPBITS_1);
+            int parity = prefs.getInt("parity", UsbSerialPort.PARITY_NONE);
+
             Bundle args = new Bundle();
             args.putInt("device", item.device.getDeviceId());
             args.putInt("port", item.port);
             args.putInt("baud", baudRate);
+            args.putInt("dataBits", dataBits);
+            args.putInt("stopBits", stopBits);
+            args.putInt("parity", parity);
             Fragment fragment = new TerminalFragment();
             fragment.setArguments(args);
             getParentFragmentManager().beginTransaction().replace(R.id.fragment, fragment, "terminal").addToBackStack(null).commit();
